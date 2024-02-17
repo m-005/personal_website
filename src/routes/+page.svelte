66d49485ff77052dc30dce84e838ff7cf8e5d810
mw005 <montezwalk@gmail.com>
2024-02-17 15:57:21 -0500

    Update page layout, footer, and navigation components; rename projects to blog

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 		(b. 1996)<br /><br />
 		An artist, technologist, and entrepreneur building global products guided by communication, design,
 		and harmony.<br /><br />
-		Building <a href="projects" rel="history"><u>OBJECTS</u></a> to bring personalized design for
+		Building <a href="https://objects.vision"><u>OBJECTS</u></a> to bring personalized design to
 		everyone. Built products for Instagram, Facebook, Meta, Coinbase, and XMTP Labs.<br /><br />
 	</bodycopy>
 </div>
@@ -16,7 +16,7 @@
 		justify-content: flex-end;
 		flex-direction: column;
 		flex-grow: 1;
-		
+
 		@media screen and (min-width: 768px) {
 			max-width: 70%;
 		}
--- src/routes/Nav.svelte
+++ src/routes/Nav.svelte
@@ -1,9 +1,9 @@
 <nav>
-	<h2><a href="/projects">{'Projects'.toUpperCase()}</a></h2>
+	<h2><a href="https://paragraph.xyz/@montez">{'Blog'.toUpperCase()}</a></h2>
 	<h2>
 		<a href="https://are.na/montez" target="_blank" rel="noopener noreferrer"
-			>{'Archive'.toUpperCase()}</a
-		>
+			>{'Archive'.toUpperCase()}
+		</a>
 	</h2>
 	<h2><a href="mailto:email@montez.cloud">{'Contact'.toUpperCase()}</a></h2>
 </nav>
